b5055099e06b22d22b611f3b3843f859a62e4439
Auwate <auwate1@gmail.com>
2024-04-01 11:20:52 -0400

    Added unit test for custom command.

--- status/tests/test_models.py
+++ status/tests/test_models.py
@@ -1,9 +1,18 @@
 import pytest
 from django.db import transaction, IntegrityError
-
+from django.test import TestCase
+from django.core.management import call_command
+from status.models import Status
 from status.models import Service
 
 pytestmark = pytest.mark.django_db
+
+
+class TestCommands(TestCase):
+
+    def test_create_initial_objects_creates_five_objects (self):
+        call_command('create_initial_objects')
+        assert Status.objects.count() == 5
 
 
 class TestService(object):
